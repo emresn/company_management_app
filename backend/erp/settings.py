04e983a2383c2adaaa776284e0f39d8e59464dca
Emre Sönmez <esonmez357@gmail.com>
2022-08-07 23:33:30 +0300

    chore: backend cors allowed

--- backend/erp/settings.py
+++ backend/erp/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'payment.apps.PaymentConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     
    
 ]
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -149,3 +151,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
